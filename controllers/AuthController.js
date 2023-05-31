6e8cdcc410268a92c1a2a3037038fac4f786ac9e
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-31 18:04:53 +0000

    Fixed bug in the connect routes by handling all possible exception

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -9,8 +9,23 @@ const { decodeToken, getCredentials } = require('../utils/utils');
 class AuthController {
   static async getConnect(req, res) {
     const authzHeader = getAuthzHeader(req);
+    if (!authzHeader) {
+      res.status(401).json({ error: 'Unauthorized' });
+      res.end();
+      return;
+    }
     const token = getToken(authzHeader);
+    if (!token) {
+      res.status(401).json({ error: 'Unauthorized' });
+      res.end();
+      return;
+    }
     const decodedToken = decodeToken(token);
+    if (!decodedToken) {
+      res.status(401).json({ error: 'Unauthorized' });
+      res.end();
+      return;
+    }
     const { email, password } = getCredentials(decodedToken);
     const user = await dbClient.getUser(email);
     if (!user) {
@@ -24,7 +39,7 @@ class AuthController {
       return;
     }
     const accessToken = v4();
-    await redisClient.set(`auth_${accessToken}`, user._id, 60 * 60 * 24);
+    await redisClient.set(`auth_${accessToken}`, user._id.toString('utf8'), 60 * 60 * 24);
     res.json({ token: accessToken });
     res.end();
   }
@@ -60,14 +75,12 @@ class AuthController {
       return;
     }
     const id = await redisClient.get(`auth_${token}`);
-    console.log(id);
     if (!id) {
       res.status(401).json({ error: 'Unauthorized' });
       res.end();
       return;
     }
     const user = await dbClient.getUserById(id);
-    console.log(user);
     if (!user) {
       res.status(401).json({ error: 'Unauthorized' });
       res.end();
