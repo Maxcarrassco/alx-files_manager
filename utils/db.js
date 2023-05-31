ea8b5f8344890122785675e6329d3b24690f3b11
Maxwell D. Dorliea <deboy170@gmail.com>
2023-05-31 16:57:24 +0000

    Implemented all the auth routes for basic auth

--- utils/db.js
+++ utils/db.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const { MongoClient } = require('mongodb');
-const pwdHashed = require('./utils');
+const mongo = require('mongodb');
+const { pwdHashed } = require('./utils');
 
 class DBClient {
   constructor() {
@@ -39,10 +40,28 @@ class DBClient {
     return user;
   }
 
-  async userExist(email) {
+  async getUser(email) {
     await this.client.connect();
     const user = await this.client.db(this.database).collection('users').find({ email }).toArray();
-    if (user.length) {
+    if (!user.length) {
+      return null;
+    }
+    return user[0];
+  }
+
+  async getUserById(id) {
+    const _id = new mongo.ObjectID(id);
+    await this.client.connect();
+    const user = await this.client.db(this.database).collection('users').find({ _id }).toArray();
+    if (!user.length) {
+      return null;
+    }
+    return user[0];
+  }
+
+  async userExist(email) {
+    const user = await this.getUser(email);
+    if (user) {
       return true;
     }
     return false;
--- utils/utils.js
+++ utils/utils.js
@@ -2,6 +2,14 @@
 
 const sha1 = require('sha1');
 
-const pwdHashed = (pwd) => sha1(pwd);
-
-module.exports = pwdHashed;
+export const pwdHashed = (pwd) => sha1(pwd);
+export const getAuthzHeader = (req) => req.headers.authorization;
+export const getToken = (authzHeader) => authzHeader.substring(6);
+export const decodeToken = (token) => Buffer.from(token, 'base64').toString('utf8');
+export const getCredentials = (decodedToken) => {
+  const [email, password] = decodedToken.split(':');
+  if (!email || !password) {
+    return null;
+  }
+  return { email, password };
+};
